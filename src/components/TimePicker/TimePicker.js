541ccd1210bcc39115f265887477de022f6d2d43
Eve Shi <qianyiwork@gmail.com>
2018-09-13 02:36:50 +0800

    config scripts

--- src/components/TimePicker/TimePicker.js
+++ src/components/TimePicker/TimePicker.js
@@ -21,6 +21,10 @@ export class TimePicker extends PureComponent {
       minuteInit,
     } = this.props;
 
+    if (hourInit == null) {
+      return;
+    }
+
     this.setState({
       hourPicked: hourInit,
       minutePicked: minuteInit,
